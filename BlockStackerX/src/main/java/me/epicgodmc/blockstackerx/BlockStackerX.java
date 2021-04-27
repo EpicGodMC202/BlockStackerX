252529e2785be1b08eb963908fb37afb82169b20
Bert <bertgarretsen@outlook.com>
2021-04-27 11:45:05 +0200

    Initial commit

---
+++ BlockStackerX/src/main/java/me/epicgodmc/blockstackerx/BlockStackerX.java
@@ -0,0 +1,21 @@
+package me.epicgodmc.blockstackerx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class BlockStackerX extends JavaPlugin {
+
+    private final Logger logger = Logger.getLogger("Minecraft");
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
